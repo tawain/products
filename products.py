9bbf51036bb37ff4e3861d038921c3c504269876
tawain <pinchuan0813@gmail.com>
2021-02-12 21:48:35 +0800

    add header

--- products.py
+++ products.py
@@ -18,7 +18,10 @@ for p in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
-with open('products.csv', 'w') as f: # r(read)讀取模式/w(write)寫入模式
+with open('products.csv', 'w', encoding='utf-8') as f: # r(read)讀取模式/w(write)寫入模式
+	f.write('商品,價格\n') 
+	# 寫入和讀取都牽扯到編碼(encoding)。所以加入encoding='utf-8'。
+	# 沒有這行也沒變亂碼。可能因為不是用excel開csv，所以沒亂碼的問題。詳見講座64
 	for p in products:
 		f.write(str(p[0]) + ',' + str(p[1]) + '\n') # ,做區隔；\n換行符號
 		#f寫入str + str + str + str
